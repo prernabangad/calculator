ada192d2a85006d4e87bd3d542201a923537f5f8
prernabangad <email address>
2020-08-26 19:51:10 +0530

    Updates

---
+++ precache-manifest.eac793e1124750547af24b509f07a060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64302f84e03cbaf54591846fb4400db",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "09795cb414a2573a7598",
+    "url": "/calculator/static/css/main.c8d6ecac.chunk.css"
+  },
+  {
+    "revision": "fdbfe1e5a0a5835774c3",
+    "url": "/calculator/static/js/2.7fbf350a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.7fbf350a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09795cb414a2573a7598",
+    "url": "/calculator/static/js/main.002cc995.chunk.js"
+  },
+  {
+    "revision": "2f771d3db25210da76fb",
+    "url": "/calculator/static/js/runtime-main.47932f8d.js"
+  }
+]);
